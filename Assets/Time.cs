64ee91fe60468f073d8d08ce6833987152a667e4
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-06-15 14:17:25 -0300

    d

--- Assets/Time.cs
+++ Assets/Time.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Time : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
